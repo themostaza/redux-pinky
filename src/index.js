06b8c5e468d7c10a3a9ca2215bc69b2528d6d488
mmazzarolo <mazzarolomatteo@gmail.com>
2017-02-14 16:33:02 +0100

    Added flowtype definitions and _REQUEST check

--- src/index.js
+++ src/index.js
@@ -1,72 +1,81 @@
-import uuid from 'uuid'
+/* @flow */
+import uuid from 'uuid';
 
-const handleEventHook = (meta, hook, ...args) => {
+export type AsyncParameters = {
+  payload?: any,
+  promise: Promise<any>,
+  meta?: {
+    onStart?: (payload: any, state: any) => any,
+    onFinish?: (result: boolean, state: any) => any,
+    onFailure?: (error: any, state: any) => any,
+  },
+};
+
+const handleEventHook = (meta: Object, hook: string, ...args) => {
   if (meta && meta[hook] && typeof meta[hook] === 'function') {
     // We want to make sure that an "eventHook" doesn't cause a dispatch to fail, so we wrap it
     // with a try..catch. In dev, we `console.error` which will result in a redbox.
     try {
-      meta[hook](...args)
+      meta[hook](...args);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
   }
-}
+};
 
-export default (store) => (next) => (action) => {
-  // A common use case for redux-thunk is to conditionally dispatch an action.
-  // By allowing for null, we satisfy this use case without people having to use redux-thunk.
-  if (action === null) {
-    return null
-  }
-
-  const isPromise = action.promise && typeof action.promise.then === 'function'
+export default (store: any) => (next: any) => (action: any) => {
+  const isPromise = action.promise && typeof action.promise.then === 'function';
 
   // This is the "vanilla redux" pathway. These are plain old actions that will get sent to reducers
   if (!isPromise) {
-    return next(action)
+    return next(action);
   }
 
   // This is the convention-based promise middleware.
   // Ideally, all "async actions" would go through this pathway.
-  const { promise, type, payload, meta } = action
+  const { promise, type, payload, meta } = action;
+
+  // The first dispatched action type will have a "_REQUEST" suffix, so if the original action type
+  // already ends with "_REQUEST" it is sliced here.
+  const hasRequestType = type.substr(type.length - 8) === '_REQUEST';
+  const actionType = hasRequestType ? type.slice(0, -8) : type;
 
   // It is sometimes useful to be able to track the actions and associated promise lifecycle with a
   // sort of unique identifier.
-  const transactionId = uuid.v4()
-  const initialPayload = payload
+  const transactionId = uuid.v4();
+  const initialPayload = payload;
 
   // Dispatch the _REQUEST type action
   store.dispatch({
-    type: `${type}_REQUEST`,
+    type: `${actionType}_REQUEST`,
     payload,
-    meta: { ...meta, transactionId }
-  })
-  handleEventHook(meta, 'onStart', payload, store.getState)
+    meta: { ...meta, transactionId },
+  });
+  handleEventHook(meta, 'onStart', payload, store.getState);
 
   // Prepare the _SUCCESS type action
-  const success = (data) => {
+  const success = data => {
     store.dispatch({
-      type: `${type}_SUCCESS`,
+      type: `${actionType}_SUCCESS`,
       payload: data,
-      meta: { ...meta, initialPayload, transactionId }
-    })
-    handleEventHook(meta, 'onSuccess', data, store.getState)
-    handleEventHook(meta, 'onFinish', true, store.getState)
-  }
+      meta: { ...meta, initialPayload, transactionId },
+    });
+    handleEventHook(meta, 'onFinish', true, store.getState);
+  };
 
   // Prepare the _FAILURE type action
-  const failure = (error) => {
+  const failure = error => {
     store.dispatch({
-      type: `${type}_FAILURE`,
+      type: `${actionType}_FAILURE`,
       payload: error,
       error: true,
-      meta: { ...meta, initialPayload, transactionId }
-    })
-    handleEventHook(meta, 'onFailure', error, store.getState)
-    handleEventHook(meta, 'onFinish', false, store.getState)
-  }
+      meta: { ...meta, initialPayload, transactionId },
+    });
+    handleEventHook(meta, 'onFailure', error, store.getState);
+    handleEventHook(meta, 'onFinish', false, store.getState);
+  };
 
   // Return the promise.
   // In this case, when users dispatch an action with a promise payload, they can `.then` it.
-  return promise.then(success, failure)
-}
+  return promise.then(success, failure);
+};
